67c03755fe32580af383e7fca1d1831436eef365
OcimaSagledam <justcurlygames@gmail.com>
2021-12-27 16:39:44 +0100

    Added the first part of game

--- engine.js
+++ engine.js
@@ -1,15 +1,16 @@
 const Engine = function (time_step, update, render){
 
+    this.accumulated_time = 0; //Amount of time that's accumulated since the last update
     this.animation_frame_request = undefined,
-    this.time = undefined,
-    this.time_step = time_step,
+    this.time = undefined, //The most recent timestamp of loop execution
+    this.time_step = time_step, //1000/30 = 30 frames per second
 
     this.update = false;
 
     this.update = update;
     this.render = render;
 
-    this.run = function(time_stamp){
+    this.run = function(time_stamp){ //This is once cycle of the game loop
         this.accumulated_time += time_stamp - this.time;
         this.time = time_stamp;
 
